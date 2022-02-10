03bdd0bc97e46b8f636be579cc89fe3b60540a76
void404 <wojciech.nawa@gmail.com>
2022-02-10 18:05:52 +0100

    Add linux support

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
-#include <cmath>
 #include <fstream>
 #include <vector>
 
-using namespace std;
+#ifdef _WIN32
+#define mapPath "C:\\Users\\marty\\Desktop\\mapa.txt"
+#endif
 
+#ifdef linux
+#define mapPath "./mapa.txt"
+#endif
+
+using namespace std;
 
 int main() {
     fstream mapa;
-    mapa.open("C:\\Users\\marty\\Desktop\\mapa.txt", ios::in);
+    mapa.open(mapPath, fstream::in);
 
-    if(!mapa.good()) {
-        cout << "Brak mapy" << endl;
+    if (!mapa.good()) {
+        cout << "Brak mapy w " << mapPath << endl;
         return 0;
     }
 
     vector<string> linie;
     string linia;
 
-    while(getline(mapa,linia)) {
+    while (getline(mapa, linia)) {
         linie.push_back(linia);
     }
 
     mapa.close();
 
-    int najdluzszy =0;
+    int najdluzszy = 0;
     int sumMapa = 0;
 
-    for (int i = 0; i<linie.size(); ++i) {
-        if (linie[i].length()>najdluzszy) {
+    for (int i = 0; i < linie.size(); ++i) {
+        if (linie[i].length() > najdluzszy) {
             najdluzszy = linie[i].length();
         }
         sumMapa += linie[i].length();
     }
 
-    double wynik = (sumMapa*100)/(najdluzszy*linie.size());
+    double wynik = (sumMapa * 100) / (najdluzszy * linie.size());
 
     if (wynik >= 80) {
         cout << "ok" << endl;
